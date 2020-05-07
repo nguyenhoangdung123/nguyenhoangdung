47fe27a65bb0ec589a7cc7254371b3a9535411fe
nguyenhoangdung123 <54362656+nguyenhoangdung123@users.noreply.github.com>
2020-05-07 15:07:38 +0700

    Revert "Delete muatrongnam.py"
    This reverts commit 423d7e9f59d22d35a984ad5ad4ddcaf4af7f5750.

---
+++ muatrongnam.py
@@ -0,0 +1,12 @@
+yob = int(input("a month of year"))
+season =  yob*3
+print(season)
+
+if :
+     print("spring")
+elif :
+    print("summer")
+elif :
+    print("autumn")
+else:
+    print("winter")
